e7e6a75eeeefb7e19108f170dc494a9c4d7835f7
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-08-21 13:33:55 -0300

    criei uma pasta com react e só adicionei um componente NAV

---
+++ pagina-com-react/src/componentes/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return ( 
+        <>
+            <a href="#" id="home">Home</a>
+            <a href="#" id="sobre">Sobre</a>
+            <a href="#">Contato</a>
+        </>
+     );
+}
+
+export default Nav;
