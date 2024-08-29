55aea713cfa974d835fb1468ce483400f878f8a2
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-08-28 21:25:49 -0300

    a

---
+++ pagina-com-reactt/src/componentes/Box/index.jsx
@@ -0,0 +1,11 @@
+export default function Box(props) {
+    <div className={props.classNomeRede}>
+        <div className="numero-icone">
+            <h1>01</h1>
+            <img src={LogoYoutube} alt="imagem do ícone do Youtube"/>
+        </div>
+        
+        <h2>Youtube</h2>
+        <p>Produza conteúdo cativante e conquiste seu público com vídeos de alta qualidade no maior palco digital do mundo.</p>
+    </div>
+}
